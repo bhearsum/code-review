a37f7c307ada1ed33a6bf99872aab31189213f96
Ben Hearsum <ben@mozilla.com>
2026-09-21 14:04:54 -0400

    feat: add support for suffixes in namespaces that reviewbot tasks publish to

--- bot/tests/test_index.py
+++ bot/tests/test_index.py
@@ -71,6 +71,54 @@ def test_taskcluster_index(mock_config, mock_workflow):
     assert "indexed" in args["data"]
 
 
+def test_taskcluster_index_with_suffix(mock_config, mock_workflow):
+    """
+    Test the Taskcluster indexing API
+    by mocking an online taskcluster state
+    """
+
+    mock_config.taskcluster = TaskCluster("/tmp/dummy", "12345deadbeef", 0, False)
+    mock_workflow.index_service = mock.Mock()
+    rev = MockPhabricatorRevision(
+        namespaces=["mock.1234"],
+        details={"id": "1234", "someData": "mock", "state": "done"},
+        repository="test-repo",
+    )
+    mock_workflow.index(rev, namespace_suffix="suffixtest", test="dummy")
+
+    assert mock_workflow.index_service.insertTask.call_count == 2
+    calls = mock_workflow.index_service.insertTask.call_args_list
+
+    # First call with namespace
+    namespace, args = calls[0][0]
+    assert namespace == "project.relman.test.code-review.mock.1234.suffixtest"
+    assert args["taskId"] == "12345deadbeef"
+    assert args["data"]["test"] == "dummy"
+    assert args["data"]["id"] == "1234"
+    assert args["data"]["source"] == "try"
+    assert args["data"]["try_task_id"] == "remoteTryTask"
+    assert args["data"]["try_group_id"] == "remoteTryGroup"
+    assert args["data"]["repository"] == "test-repo"
+    assert args["data"]["someData"] == "mock"
+    assert "indexed" in args["data"]
+
+    # Second call with sub namespace
+    namespace, args = calls[1][0]
+    assert (
+        namespace
+        == "project.relman.test.code-review.mock.1234.suffixtest.12345deadbeef"
+    )
+    assert args["taskId"] == "12345deadbeef"
+    assert args["data"]["test"] == "dummy"
+    assert args["data"]["id"] == "1234"
+    assert args["data"]["source"] == "try"
+    assert args["data"]["try_task_id"] == "remoteTryTask"
+    assert args["data"]["try_group_id"] == "remoteTryGroup"
+    assert args["data"]["repository"] == "test-repo"
+    assert args["data"]["someData"] == "mock"
+    assert "indexed" in args["data"]
+
+
 def test_index_autoland(
     mock_autoland_task,
     mock_phabricator,
